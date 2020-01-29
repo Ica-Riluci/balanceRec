45b38b79fa0de489f67f53d6d36421b4f9d7909b
Ica Riluci <icariluci@Icas-MacBook-Pro.local>
2020-01-29 18:40:14 +0800

    add rec backend

--- src/recService/recorder/models.py
+++ src/recService/recorder/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class BalRec(models.Model):
-    date = models.DateField()
+    datex = models.DateField()
     recType = models.PositiveIntegerField()
     disType = models.PositiveIntegerField()
     income = models.FloatField()
-    outcome = models.FloatField()
+    output = models.FloatField()
     detail = models.CharField(max_length=255)
--- src/recService/recorder/views.py
+++ src/recService/recorder/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from models import BalRec
+from datetime import date, datetime
+import json
 
 # Create your views here.
+
+class AddRec(generic.View):
+    def post(self, request):
+        data = json.loads(request.body)
+        create_date = datetime.strptime(data['year'] + '-' + data['month'] + '-' + data['date'], '%Y-%m-%d')
+        new_rec = BalRec(datex=create_date, recType=data['type'], disType=data['dist'], income=data['inc'], output=data['out'], detail=data['detail'])
+        new_rec.save()
+        return HttpResponse(json.dumps(json.load({"valid":True, "data":data})), content_type="application/json")
