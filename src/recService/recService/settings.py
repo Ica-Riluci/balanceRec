32c7d46f3182739b43a267d4362ad5873f8cc5fb
Ica Riluci <icariluci@Icas-MacBook-Pro.local>
2020-01-29 11:22:37 +0800

    install cross-site pack

--- src/recService/recService/settings.py
+++ src/recService/recService/settings.py
@@ -37,13 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # enable cross-site requriement
+    'server',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # enable cross-site requriement
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -112,6 +117,32 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# enable cross-site requriement
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = ('localhost:3000')
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTION',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW'
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with'
+)
 
 
 # Static files (CSS, JavaScript, Images)
